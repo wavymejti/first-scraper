8d7a4ac5478d584b9e25af065c9aa69dc93046cd
wavymejti <mateusztheszymkowiak@gmail.com>
2024-08-03 19:32:00 +0200

    venv installes

--- scraper.py
+++ scraper.py
@@ -1,1 +1,7 @@
-print("hello world")
+#Will use requests// no java so sodium is over
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('https://quotes.toscrape.com/')
+print(page)
+
